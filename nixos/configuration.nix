2a022523b33e6597ddfc62d7341ca122e5555160
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2023-07-04 22:45:04 +0300

    1

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,216 +10,211 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.supportedFilesystems = [ "ntfs" ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.supportedFilesystems = [ "ntfs" ];
+    networking.hostName = "nixos"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Enable networking
+    networking.networkmanager.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    # Set your time zone.
+    time.timeZone = "Europe/Moscow";
 
-  # Set your time zone.
-  time.timeZone = "Europe/Moscow";
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_GB.UTF-8";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "ru_RU.UTF-8";
+      LC_IDENTIFICATION = "ru_RU.UTF-8";
+      LC_MEASUREMENT = "ru_RU.UTF-8";
+      LC_MONETARY = "ru_RU.UTF-8";
+      LC_NAME = "ru_RU.UTF-8";
+      LC_NUMERIC = "ru_RU.UTF-8";
+      LC_PAPER = "ru_RU.UTF-8";
+      LC_TELEPHONE = "ru_RU.UTF-8";
+      LC_TIME = "ru_RU.UTF-8";
+    };
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ru_RU.UTF-8";
-    LC_IDENTIFICATION = "ru_RU.UTF-8";
-    LC_MEASUREMENT = "ru_RU.UTF-8";
-    LC_MONETARY = "ru_RU.UTF-8";
-    LC_NAME = "ru_RU.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "ru_RU.UTF-8";
-  };
+    # Enable the X11 windowing system.
+    services.xserver = 
+      {
+      	enable = true;
+      	displayManager = {
+      		sddm.enable = true;
+      		# defaultSession = "none+awesome";
+      	};
 
-  # Enable the X11 windowing system.
-  services.xserver = 
-	{
-		enable = true;
-		displayManager = {
-			sddm.enable = true;
-			# defaultSession = "none+awesome";
-		};
-
-		desktopManager.plasma5.enable = true;
-		windowManager.awesome = {
-			enable = true;
-			luaModules = with pkgs.luaPackages; [
-				luarocks # is the package manager for Lua modules
-				luadbi-mysql # Database abstraction layer
-			];
-		};
-	};
+      	desktopManager.plasma5.enable = true;
+      	windowManager.awesome = {
+      		enable = true;
+      		luaModules = with pkgs.luaPackages; [
+      			luarocks # is the package manager for Lua modules
+      			luadbi-mysql # Database abstraction layer
+      		];
+      	};
+      };
 
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "gb";
-    xkbVariant = "";
-  };
+    # Configure keymap in X11
+    services.xserver = {
+      layout = "gb";
+      xkbVariant = "";
+    };
 
-  # Configure console keymap
-  console.keyMap = "uk";
+    # Configure console keymap
+    console.keyMap = "uk";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    # Enable sound with pipewire.
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+    # Enable touchpad support (enabled default in most desktopManager).
+    # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  programs.fish.enable = true;
-  users.defaultUserShell = pkgs.fish;
-  users.users.badatkovaak = {
-    isNormalUser = true;
-    description = "badatkovaak";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-      kate
-    #  thunderbird
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    programs.fish.enable = true;
+    users.defaultUserShell = pkgs.fish;
+    users.users.badatkovaak = {
+      isNormalUser = true;
+      description = "badatkovaak";
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [
+        firefox
+        kate
+      #  thunderbird
+      ];
+    };
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.localBinInPath = true;
+    environment.systemPackages = with pkgs; [
+    	vim
+        neovim
+        vscodium
+        git
+        htop
+        neofetch
+        docker
+        docker-compose
+        wget
+        appimage-run
+        ripgrep
+        parted
+        fish
+        awesome
+        dmenu
+        nitrogen
+        compton
+        alacritty
+        kitty
+        telegram-desktop
+        unzip
+        gcc
+        gnumake
+         #tmux
+        nodejs
+        nodePackages.purescript-language-server
+        bun
+        deno
+        haxe
+         # neko
+        ghc
+        cargo
+        rustc
+        wasmer
+        go
+        elixir
+        elixir-ls
+        dotnet-sdk
+        julia-bin
+        purescript
+        elmPackages.elm
+        elmPackages.elm-language-server
+        #(elmPackages. [ elm-live elm-spa elm-test elm-pages elm-format elm-language-server ] 
+        stylish-haskell
+        haskell-language-server
+         #poetry
+        (python311.withPackages(ps: with ps; [
+        	python-lsp-server 
+         	pylint 
+         	autopep8 
+         	pytelegrambotapi 
+         	requests 
+         	django
+         		]))
+        pypy3
+        stylua
+        lua-language-server
+        gmp
+        ncurses
+        qutebrowser
+        xclip
     ];
-  };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    programs.neovim.enable = true;
+    programs.neovim.defaultEditor = true;
+    services.flatpak.enable = true;
+    programs.git = {
+      enable = true;
+      #userName  = "badatkovaak";
+      #userEmail = "122653634+badatkovaak@users.noreply.github.com";
+    };
+    virtualisation.docker.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.localBinInPath = true;
-  environment.systemPackages = with pkgs; [
-  	vim
-	neovim
-	git
-	htop
-	neofetch
-	docker
-	docker-compose
-	wget
-	appimage-run
-	ripgrep
-	fish
-	awesome
-	dmenu
-	nitrogen
-	compton
-	alacritty
-	kitty
-	telegram-desktop
-	unzip
-	gcc
-	gnumake
-	#tmux
-	nodejs
-	bun
-	deno
-	haxe
-	ghc
-	cargo
-	rustc
-	wasmer
-	elixir
-	elixir-ls
-	dotnet-sdk
-	julia-bin
-	elmPackages.elm
-	elmPackages.elm-language-server
-	#(elmPackages. [ elm-live elm-spa elm-test elm-pages elm-format elm-language-server ] 
-	stylish-haskell
-	haskell-language-server
-	#poetry
-	(python311.withPackages(ps: with ps; [
-		python-lsp-server 
-		pylint 
-		autopep8 
-		pytelegrambotapi 
-		requests 
-		django
-		#(buildPythonPackage rec {
-		#	pname = "berserk";
-		#	version = "0.12.6";
-		#	format = "pyproject";
-		#	src = fetchPypi {
-		#		inherit pname version;
-		#		sha256 = "d35b5ee1239b135a401d705e2a37681f693bcb05c340d2320b0a1811ab3f8c1d";};
-		#	doCheck = false;
-		#	propagatedBuildInputs = [];
-		#	})
-	]))
-	#(python311.withPackages(ps: with ps; [ python-lsp-server pylint autopep8 pytelegrambotapi requests django ]))
-	pypy3
-	stylua
-	lua-language-server
-	gmp
-	ncurses
-	qutebrowser
-	xclip
-  ];
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 
-  programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
-  services.flatpak.enable = true;
-  programs.git = {
-    enable = true;
-    #userName  = "badatkovaak";
-    #userEmail = "122653634+badatkovaak@users.noreply.github.com";
-  };
-  virtualisation.docker.enable = true;
+    # List services that you want to enable:
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
 
-  # List services that you want to enable:
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "23.05"; # Did you read the comment?
 
 }
